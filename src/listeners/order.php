77364d966b388c2599026f37c34f2847857ebe21
EVOKE <evoke.lektrique@gmail.com>
2023-02-14 04:41:02 +0330

    Fixed stuff

--- src/listeners/order.php
+++ src/listeners/order.php
@@ -11,12 +11,7 @@ if (!defined('ABSPATH')) exit;
 class Order {
 
    public function __construct() {
-      add_action('woocommerce_new_order', [$this, "listen_new_status"]);
       add_action('woocommerce_order_status_changed', [$this, "listen_change_status"], 10, 3);
-   }
-
-   public function listen_new_status($order_id) {
-      error_log(print_r(["CALLED woocommerce_new_order", "NEW ORDER", "ORDER_ID" => $order_id], true));
    }
 
    /**
@@ -28,13 +23,91 @@ class Order {
     * @return void
     */
    public function listen_change_status($order_id, $old_status, $new_status) {
-      error_log(print_r(["CALLED woocommerce_order_status_changed", "NEW ORDER - STATUS CHANGED FROM [$old_status] TO [$new_status]", "ORDER_ID" => $order_id], true));
+      $data = [
+         "customer" => [
+            "username" => null,
+            "email" => null,
+            "phone" => null,
+            "first_name" => null,
+            "last_name" => null,
+         ],
+         "order" => [
+            "id" => null,
+            "status" => null,
+            "old_status" => null,
+            "new_status" => null,
+            "price" => null,
+            "items" => [],
+         ],
+         "note" => [
+            "type" => null,
+            "content" => null,
+         ],
+      ];
 
-      // $response = new \wtm_plugin\Publishers\Post(
-      //    $parsed_post,
-      //    $node,
-      //    $is_update,
-      //    $is_delete
-      // );
+      $order = wc_get_order($order_id);
+
+      // Order and its items in general
+      $data['order']['id'] = $order_id;
+      $data['order']['status'] = $order->get_status();
+      $data['order']['old_status'] = $old_status;
+      $data['order']['new_status'] = $new_status;
+      $data['order']['price'] = $order->get_total();
+      $data['order']['items'] = $this->get_variations($order);
+
+      // Notes
+      $data['note']['content'] = $order->get_customer_note();
+      $data['note']['type'] = "customer";
+
+      // Customer
+      $user = $order->get_user();
+      $data['customer']['username'] = $user->user_login;
+      $data['customer']['first_name'] = $order->get_billing_first_name();
+      $data['customer']['last_name'] = $order->get_billing_last_name();
+      $data['customer']['phone'] = $order->get_billing_phone();
+      $data['customer']['email'] = $order->get_billing_email();
+
+      $request = new \wtm_plugin\Publishers\Order($data);
+
+      error_log(print_r([$request->response, json_decode($request->response->getBody(), true)], true));
+   }
+
+
+   /**
+    * Retrieve an array of order items varations.
+    *
+    * @param \WC_Order $order
+    * @return array
+    */
+   private function get_variations(\WC_Order $order): array {
+      $variation_names = [];
+
+      foreach ($order->get_items() as $item_id => $item) {
+
+         // Get the WC_Product Object
+         $product = $item->get_product();
+         $product_title = $product->get_title();
+         $variation_names[$product_title] = [];
+         $variation_names[$product_title]["quantity"] = $item->get_quantity();
+
+         if ($product->is_type('variation')) {
+            $variation_id = $item->get_variation_id();
+            $variation = new \WC_Product_Variation($variation_id);
+            $attributes = $variation->get_attributes();
+
+            if ($attributes) {
+               foreach ($attributes as $key => $value) {
+                  $variation_key = str_replace('-', ' ', $key);
+
+                  $variation_names[$product_title]["variations"][] = [
+                     "key" => urldecode($variation_key),
+                     "value" => urldecode($value)
+                  ];
+               }
+            }
+         }
+      }
+
+      return $variation_names;
    }
 }
--- src/publishers/order.php
+++ src/publishers/order.php
@@ -3,10 +3,10 @@
 namespace wtm_plugin\Publishers;
 
 use GuzzleHttp\Client;
-use wtm_plugin\Helper;
+use GuzzleHttp\RequestOptions;
 
 // Exit if accessed directly
-if(!defined( 'ABSPATH' )) exit;
+if (!defined('ABSPATH')) exit;
 
 /**
  * Handle publishing orders
@@ -14,64 +14,37 @@ if(!defined( 'ABSPATH' )) exit;
 class Order {
 
    private $route;
+   private $api_key;
+   public $response;
 
-   public function __construct(array $body, object $target_node, bool $is_update, bool $is_delete) {
-      // $get_target_node = \wtm_plugin\Tables\Nodes\Functions::get_by("hash", $target_node->hash);
-      // $parsed_target_node_url = Helper::generate_url_from_node($target_node);
-      // $response = $this->perform($body, $parsed_target_node_url, $is_update, $is_delete);
-      // if(Helper::is_json($response)) {
-      //    $response = json_decode($response, true);
-      //    \wtm_plugin\Log::write(
-      //       sprintf(__("%s", "wtm-plugin"), $response["message"]),
-      //       $get_target_node->id,
-      //       1 // Info
-      //    );
-      // } else {
-      //    $response = ["status" => false, "message" => "unknown"];
-      // }
-      // return $response;
+   public function __construct(array $data) {
+      $this->route = $this->get_route();
+      $this->api_key = $this->get_api_key();
+
+      $client = new Client();
+      $response = $client->post($this->route, [
+         'headers' => [
+            'Authorization' => 'Bearer ' . $this->api_key
+         ],
+         RequestOptions::JSON => $data
+      ]);
+
+      $this->response = $response;
    }
 
-   public function perform(array $body, array $parsed_target_node_url, bool $is_update, bool $is_delete) {
-      // $controller      = new \wtm_plugin\Controllers\Publish();
-      // $route           = $controller->get_route();
-      // $target_node_url = $parsed_target_node_url["formed"] . $route;
+   public function perform(array $data) {
 
-      // $client = new \GuzzleHttp\Client();
-
-      // $method = "POST";
-      // if($is_delete) {
-      //    $method = "DELETE";
-      // }
-
-      // try {
-      //    $request = $client->request($method, $parsed_target_node_url["parsed"]["scheme"] . $target_node_url, [
-      //       'json' => $body
-      //    ]);
-      //    return (string) $request->getBody();
-      // } catch(\GuzzleHttp\Exception\ServerException $e) {
-      //    return new \WP_Error('request-server-error',
-      //       __(
-      //          "Couldn't establish a connection to the server or an error happened on the target server.",
-      //          'wtm-plugin'
-      //       )
-      //    );
-      // } catch(\GuzzleHttp\Exception\ClientException $e) {
-      //    $error = $e->getResponse()->getBody()->getContents();
-      //    if(Helper::is_json($error)) {
-      //       $error_array   = json_decode($error, true);
-      //       $error_code    = $error_array["code"];
-      //       $error_message = $error_array["message"];
-      //       return new \WP_Error($error_code, $error_message);
-      //    }
-
-      //    return new \WP_Error('request-client-error',
-      //       __(
-      //          "Couldn't establish a connection to the server or an error happened on the target server.",
-      //          'wtm-plugin'
-      //       )
-      //    );
-      // }
    }
 
+   private function get_route() {
+      $value = "192.168.1.105:8000/api/order/create";
+
+      return $value;
+   }
+
+   private function get_api_key() {
+      $value = "1|8I75ZEWGtr73G7YWpVJLFlmFso39iKpR3TJNzLuI";
+
+      return $value;
+   }
 }
